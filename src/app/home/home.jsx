8bd9eeebd40eec9e86119f433680e345dfd854a3
KEFFA <muthurikeffa254@gmail.com>
2025-10-24 00:29:54 +0300

    link to login and register update

--- src/app/home/home.jsx
+++ src/app/home/home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
   return (
     <div className="home_container">
       <div className="login_section">
-        <Link className="login_link" to="/login">Login</Link>  
+        <Link className="login_link" to="/home/login">Login</Link>  
       </div>
 
       <div className="landing_center">
         <h1>Manage Daily Task</h1>
         <p>Organize your daily tasks efficiently.</p>
         <p>Minimize task wastage.</p>
-        <a className="get_started_link" href="/register">Get started</a>
+        <a className="get_started_link" href="/home/register">Get started</a>
       </div>
     </div>
   );
